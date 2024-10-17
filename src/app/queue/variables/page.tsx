a7fa256abe14f4c42f04c6baa6ccfd1236eb1603
sh4dow18 <ramses.solano.arias@gmail.com>
2024-10-16 23:21:48 -0600

    add: queue variables page
    Add the Arrival Distribution Rate (λ) and the Average Service Rate (µ) inputs in new queue variables page

---
+++ src/app/queue/variables/page.tsx
@@ -0,0 +1,75 @@
+// Queue Variables Page Requirements
+import { Form, Input, Page } from "@/components";
+import { Metadata } from "next";
+// Queue Page Constants
+const TITLE = "Variables";
+const DESCRIPTION =
+  "Aquí se agregan los valores a las variables para el resultado final";
+// Queue Variables Metadata
+export const metadata: Metadata = {
+  title: TITLE,
+  description: DESCRIPTION,
+};
+// Queue Variables Page Props
+type Props = {
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+// Queue Variables Page Main Function
+function QueueVariablesPage({ searchParams }: Props) {
+  // Get Model from Param
+  const MODEL =
+    typeof searchParams["model"] === "string"
+      ? searchParams["model"]
+      : "mm1-fifo-inf-inf";
+  // Returns Queue Variables Page
+  return (
+    <Page title={TITLE} description={DESCRIPTION}>
+      <section>
+        {/* Set Values */}
+        <h2>Establecer Valores</h2>
+        {/* Variables Form */}
+        <Form
+          api="inventory/result"
+          method="GET"
+          button="Calcular"
+          modal={{
+            success: "Se han Calculado las Nuevas Variables",
+            error: "No se han podido calcular las Nuevas Variables",
+            loading: "Obteniendo Variables",
+          }}
+        >
+          <div>
+            {/* Invisible Input that has Model Code and it is Read Only */}
+            <input
+              className="invisible"
+              type="text"
+              name="model"
+              value={MODEL}
+              readOnly
+            />
+            {/* Arrival Distribution Rate Input */}
+            <Input
+              label="Tasa de Distribución de Llegada (λ)"
+              type="text"
+              name="l"
+              example="90"
+              help="Números Positivos Solamente"
+              validation="number"
+            />
+            {/* Average Service Rate Input */}
+            <Input
+              label="Tasa media de Servicio (µ)"
+              type="text"
+              name="l"
+              example="90"
+              help="Números Positivos Solamente"
+              validation="number"
+            />
+          </div>
+        </Form>
+      </section>
+    </Page>
+  );
+}
+
+export default QueueVariablesPage;

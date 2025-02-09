7cee550f7ede39eb88241a673f102fce3a1aeb62
sh4dow18 <ramses.solano.arias@gmail.com>
2025-02-08 23:18:07 -0600

    add: not found page metadata
    Add Not found page metadata

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,22 @@
 // Not Found Requirements
 import { Section } from "@/components";
+import { Metadata } from "next";
 import Link from "next/link";
+// Not Found Constants
+const TITLE = "Página No Encontrada";
+const DESCRIPTION =
+  "Lo sentimos, no se pudo encontrar la página que está buscando.";
+// Not Found Metadata
+export const metadata: Metadata = {
+  title: TITLE,
+  description: DESCRIPTION,
+};
 // Not Found Main Function
 function NotFound() {
   // Return Not Found Page
   return (
     // Not Found Main Section
-    <Section
-      preTitle="404"
-      title="Página No Encontrada"
-      description="Lo sentimos, no se pudo encontrar la página que está buscando."
-      main
-    >
+    <Section preTitle="404" title={TITLE} description={DESCRIPTION} main>
       {/* Return to Home Link */}
       <div className="flex justify-center mt-7 md:mt-2 min-[1440px]:mx-auto">
         <Link
